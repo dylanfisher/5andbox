e925764e0e2ca8d40498825440bcbf5c9980271b
dylanfisher <hi@dylanfisher.com>
2014-02-08 20:27:49 -0500

    removing node modules from repository

--- functions.php
+++ functions.php
@@ -375,8 +375,8 @@ add_action('wp_dashboard_setup', 'sandbox_remove_dashboard_widgets' );
 // AJAX Functions
 //
 
-add_action('wp_ajax_nopriv_do_ajax', 'sandbox_our_ajax_function');
-add_action('wp_ajax_do_ajax', 'sandbox_our_ajax_function');
+// add_action('wp_ajax_nopriv_do_ajax', 'sandbox_our_ajax_function');
+// add_action('wp_ajax_do_ajax', 'sandbox_our_ajax_function');
 function sandbox_our_ajax_function(){
  
    // the first part is a SWTICHBOARD that fires specific functions
