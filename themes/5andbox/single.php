411030bb26dd1cdbfc70acbbcf800990d5268f0c
dylanfisher <hi@dylanfisher.com>
2015-05-17 14:56:02 -0400

    move git repository to wp-content directory. track plugins

---
+++ themes/5andbox/single.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+  <div class="content">
+<?php the_post() ?>
+    <div id="post-<?php the_ID() ?>" <?php post_class() ?>>
+      <h2 class="entry-title"><?php the_title() ?></h2>
+      <div class="entry-content">
+<?php the_content() ?>
+      </div>
+    </div><!-- .post -->
+  </div><!-- .content -->
+<?php get_footer() ?>
+</body>
+</html>
