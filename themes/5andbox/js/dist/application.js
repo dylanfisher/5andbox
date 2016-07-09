b4112994b140ba8e0979d2834f6ec0ba60e1c2f5
dylanfisher <hi@dylanfisher.com>
2016-07-09 15:45:09 -0400

    Add breakpoint javascript to init.js

--- themes/5andbox/js/dist/application.js
+++ themes/5andbox/js/dist/application.js
@@ -3,18 +3,52 @@
 $ = jQuery;
 
 var App = {
-  window: {
-    width: $(window).width(),
-    height: $(window).height(),
-    scrollTop: $(window).scrollTop(),
-  }
+  windowWidth: $(window).width(),
+  windowHeight: $(window).height(),
+  scrollTop: $(window).scrollTop(),
 };
 
 $(window).resize(function() {
-  App.window.width  = $(window).width();
-  App.window.height = $(window).height();
+  App.windowWidth  = $(window).width();
+  App.windowHeight = $(window).height();
 });
 
 $(window).scroll(function() {
-  App.window.scrollTop = $(window).scrollTop();
+  App.scrollTop = $(window).scrollTop();
 });
+
+App.breakpoint = function(checkIfSize) {
+  var xs = 480;
+  var sm = 768;
+  var md = 992;
+  var lg = 1200;
+  var breakpoint;
+
+  if(App.windowWidth < xs) {
+    breakpoint = 'xs';
+  } else if(App.windowWidth >= md) {
+    breakpoint = 'lg';
+  } else if(App.windowWidth >= sm) {
+    breakpoint = 'md';
+  } else {
+    breakpoint = 'sm';
+  }
+
+  if(checkIfSize !== undefined) {
+    if(checkIfSize == 'xs') {
+      return App.windowWidth < xs;
+    } else if(checkIfSize == 'sm') {
+      return (App.windowWidth >= xs && App.windowWidth < sm);
+    } else if(checkIfSize == 'md') {
+      return (App.windowWidth >= sm && App.windowWidth < md);
+    } else if(checkIfSize == 'lg') {
+      return App.windowWidth >= md;
+    }
+  } else {
+    return breakpoint;
+  }
+};
+
+App.breakpoint.isMobile = function() {
+  return ( App.breakpoint('xs') || App.breakpoint('sm') );
+};
