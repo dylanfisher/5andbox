8e876afbcf9c045c96698f1ccc6d55cf07ef11a4
dylanfisher <hi@dylanfisher.com>
2017-03-27 15:51:50 -0400

    Consistent semicolons after wp functions

--- themes/5andbox/page-custom.php
+++ themes/5andbox/page-custom.php
@@ -1,12 +1,12 @@
 <?php /* Template Name: Custom Page Name */ ?>
 
-<?php the_post() ?>
+<?php the_post(); ?>
 <div class="content">
-  <div id="page-<?php the_ID() ?>" <?php post_class() ?>>
-    <h2 class="entry-title"><?php the_title() ?></h2>
+  <div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <h2 class="entry-title"><?php the_title(); ?></h2>
     <h4>find me in page-custom.php</h4>
     <div class="entry-content">
-      <?php the_content() ?>
+      <?php the_content(); ?>
     </div>
   </div><!-- .post -->
 </div><!-- .content -->
--- themes/5andbox/page.php
+++ themes/5andbox/page.php
@@ -1,9 +1,9 @@
-<?php the_post() ?>
+<?php the_post(); ?>
 <div class="content">
-  <div id="page-<?php the_ID() ?>" <?php post_class() ?>>
-    <h2 class="entry-title"><?php the_title() ?></h2>
+  <div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <h2 class="entry-title"><?php the_title(); ?></h2>
     <div class="entry-content">
-      <?php the_content() ?>
+      <?php the_content(); ?>
     </div>
   </div><!-- .post -->
 </div><!-- .content -->
