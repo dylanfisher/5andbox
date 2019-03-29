204d066048036642e4131c5fc3946c6f2be079fe
dylanfisher <hi@dylanfisher.com>
2019-03-29 17:29:39 -0400

    Better method of removing comments

--- themes/5andbox/functions/wordpress_security/disable_comments.php
+++ themes/5andbox/functions/wordpress_security/disable_comments.php
@@ -4,23 +4,49 @@
 // Remove comments
 //
 
-// Removes from admin menu
-function sandbox_remove_admin_menus() {
-  remove_menu_page( 'edit-comments.php' );
-}
-add_action( 'admin_menu', 'sandbox_remove_admin_menus' );
+// https://gist.github.com/mattclements/eab5ef656b2f946c4bfb
 
-// Removes from post and pages
-add_action('init', 'sandbox_remove_comment_support', 100);
+add_action('admin_init', function () {
+  // Redirect any user trying to access comments page
+  global $pagenow;
 
-function sandbox_remove_comment_support() {
-  remove_post_type_support( 'post', 'comments' );
-  remove_post_type_support( 'page', 'comments' );
-}
+  if ($pagenow === 'edit-comments.php') {
+    wp_redirect(admin_url());
+    exit;
+  }
 
-// Removes from admin bar
-function sandbox_admin_bar_render() {
+  // Remove comments metabox from dashboard
+  remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+  // Disable support for comments and trackbacks in post types
+  foreach (get_post_types() as $post_type) {
+    if (post_type_supports($post_type, 'comments')) {
+      remove_post_type_support($post_type, 'comments');
+      remove_post_type_support($post_type, 'trackbacks');
+    }
+  }
+});
+
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+
+// Remove comments page in menu
+add_action('admin_menu', function () {
+  remove_menu_page('edit-comments.php');
+});
+
+// Remove comments links from admin bar
+add_action('init', function () {
+  if (is_admin_bar_showing()) {
+    remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+  }
+});
+
+add_action('wp_before_admin_bar_render', function() {
   global $wp_admin_bar;
   $wp_admin_bar->remove_menu('comments');
-}
-add_action( 'wp_before_admin_bar_render', 'sandbox_admin_bar_render' );
+});
