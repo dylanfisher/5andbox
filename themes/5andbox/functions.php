93c35855fa2b3fe1945b2e5271ca8feafd8bf6c1
dylanfisher <hi@dylanfisher.com>
2019-04-01 14:22:58 -0400

    Cleaner formatting on functions file

--- themes/5andbox/functions.php
+++ themes/5andbox/functions.php
@@ -1,42 +1,42 @@
 <?php
 
 // CSS and JS script enqueues
-require_once('functions/enqueue_scripts.php');
+require_once( 'functions/enqueue_scripts.php' );
 
 // Register custom image sizes
-require_once('functions/custom_image_sizes.php');
+require_once( 'functions/custom_image_sizes.php' );
 
 // Custom post types
-foreach (glob(get_stylesheet_directory() . '/post-types/*.php') as $filename) {
+foreach ( glob( get_stylesheet_directory() . '/post-types/*.php' ) as $filename ) {
   require_once $filename;
 }
 
 // Security
-foreach (glob(get_stylesheet_directory() . '/functions/wordpress_security/*.php') as $filename) {
+foreach ( glob( get_stylesheet_directory() . '/functions/wordpress_security/*.php' ) as $filename ) {
   require_once $filename;
 }
 
 // Enable wordpress behaviors, like adding theme support for menus
-foreach (glob(get_stylesheet_directory() . '/functions/wordpress_enables/*.php') as $filename) {
+foreach ( glob( get_stylesheet_directory() . '/functions/wordpress_enables/*.php' ) as $filename ) {
   require_once $filename;
 }
 
 // Disable unwanted default wordpress behaviors
-foreach (glob(get_stylesheet_directory() . '/functions/wordpress_disables/*.php') as $filename) {
+foreach ( glob( get_stylesheet_directory() . '/functions/wordpress_disables/*.php' ) as $filename ) {
   require_once $filename;
 }
 
 // Include all functions
-foreach (glob(get_stylesheet_directory() . '/functions/functions/*.php') as $filename) {
+foreach ( glob( get_stylesheet_directory() . '/functions/functions/*.php' ) as $filename ) {
   require_once $filename;
 }
 
 // Include all filters
-foreach (glob(get_stylesheet_directory() . '/functions/filters/*.php') as $filename) {
+foreach ( glob( get_stylesheet_directory() . '/functions/filters/*.php' ) as $filename ) {
   require_once $filename;
 }
 
 // Include all shortcodes
-foreach (glob(get_stylesheet_directory() . '/functions/shortcodes/*.php') as $filename) {
+foreach ( glob( get_stylesheet_directory() . '/functions/shortcodes/*.php' ) as $filename ) {
   require_once $filename;
 }
