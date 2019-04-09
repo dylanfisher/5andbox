39b30c823b4a600a61958f5f73e4167e536995b1
dylanfisher <hi@dylanfisher.com>
2019-04-09 16:49:41 -0400

    Add default ACF fields for controlling meta tags

--- themes/5andbox/functions/filters/acf_options.php
+++ themes/5andbox/functions/filters/acf_options.php
@@ -6,3 +6,8 @@ add_filter('acf/settings/show_admin', 'sandbox_acf_show_admin');
 function sandbox_acf_show_admin( $show ) {
   return sandbox_is_local();
 }
+
+// Enable the ACF options page
+if ( function_exists('acf_add_options_page') ) {
+  acf_add_options_page();
+}
--- themes/5andbox/partials/meta_tags.php
+++ themes/5andbox/partials/meta_tags.php
@@ -1,17 +1,59 @@
-<?php // Facebook ?>
+<?php
+  $meta_title = get_the_title();
+  $meta_description = get_bloginfo('description');
+  $meta_image_url = null;
+
+  if ( class_exists('acf') ):
+    if ( !empty( get_field('sandbox_seo_description') ) ) $meta_description = get_field('sandbox_seo_description');
+    if ( !empty( get_field('sandbox_seo_image') ) ):
+      $sandbox_seo_image = get_field('sandbox_seo_image');
+    elseif ( !empty( get_field('sandbox_seo_site_featured_image', 'option') ) ):
+      $sandbox_seo_image = get_field('sandbox_seo_site_featured_image', 'option');
+    endif;
+
+    if ( !empty( $sandbox_seo_image ) ):
+      $meta_image_url = $sandbox_seo_image['sizes']['large'];
+      $meta_image_width = $sandbox_seo_image['sizes']['large-width'];
+      $meta_image_height = $sandbox_seo_image['sizes']['large-height'];
+      $meta_image_mime_type = $sandbox_seo_image['mime_type'];
+      $meta_image_alt_text = $sandbox_seo_image['alt'];
+    endif;
+  endif;
+?>
+
 <meta property="og:url" content="<?php the_permalink(); ?>">
-<!-- <meta property="og:image" content="{{imageUrl}}"> -->
-<meta property="og:description" content="<?php echo get_bloginfo('description'); ?>">
-<meta property="og:title" content="<?php the_title(); ?>">
+<meta itemprop="name" content="<?php $meta_title; ?>">
+
+<?php if ( !empty( $meta_image_url ) ): ?>
+  <meta property="og:image" content="<?php echo $meta_image_url; ?>">
+  <meta itemprop="image" content="<?php echo $meta_image_url; ?>">
+  <meta name="twitter:image" content="<?php echo $meta_image_url; ?>">
+
+  <?php if ( !empty( $meta_image_width ) ): ?>
+    <meta property="og:image:width" content="<?php echo $meta_image_width; ?>">
+  <?php endif; ?>
+
+  <?php if ( !empty( $meta_image_height ) ): ?>
+    <meta property="og:image:height" content="<?php echo $meta_image_height; ?>">
+  <?php endif; ?>
+
+  <?php  if ( !empty( $meta_image_mime_type ) ): ?>
+    <meta property="og:image:type" content="<?php echo $meta_image_mime_type; ?>">
+  <?php endif; ?>
+
+  <?php  if ( !empty( $meta_image_alt_text ) ): ?>
+    <meta property="og:image:alt" content="<?php echo $meta_image_alt_text; ?>">
+  <?php endif; ?>
+<?php endif; ?>
+
+<meta property="og:description" content="<?php echo $meta_description ?>">
+<meta itemprop="description" content="<?php echo $meta_description ?>">
+
+<meta property="og:title" content="<?php echo $meta_title; ?>">
 <meta property="og:site_name" content="<?php echo get_bloginfo('name'); ?>">
 <meta property="og:see_also" content="<?php echo home_url('/'); ?>">
-<?php // Google ?>
-<meta itemprop="name" content="<?php the_title(); ?>">
-<meta itemprop="description" content="<?php echo get_bloginfo('description'); ?>">
-<!-- <meta itemprop="image" content="{{imageUrl}}"> -->
-<?php // Twittter ?>
+
 <meta name="twitter:card" content="summary">
 <meta name="twitter:url" content="<?php the_permalink(); ?>">
-<meta name="twitter:title" content="<?php the_title(); ?>">
-<meta name="twitter:description" content="<?php echo get_bloginfo('description'); ?>">
-<!-- <meta name="twitter:image" content="{{imageUrl}}"> -->
+<meta name="twitter:title" content="<?php $meta_title; ?>">
+<meta name="twitter:description" content="<?php echo $meta_description ?>">
