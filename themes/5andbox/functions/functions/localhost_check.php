e02c3c34a450d39290b2932995aaa235130793e7
dylanfisher <hi@dylanfisher.com>
2020-04-22 22:03:48 -0400

    Updates

--- themes/5andbox/functions/functions/localhost_check.php
+++ themes/5andbox/functions/functions/localhost_check.php
@@ -3,6 +3,14 @@
 // Check if running on localhost
 function sandbox_is_local() {
   $localhost_whitelist = array( '127.0.0.1', '::1' );
+  $remote_address = $_SERVER['REMOTE_ADDR'];
+  $is_local = false;
 
-  return in_array( $_SERVER['REMOTE_ADDR'], $localhost_whitelist );
+  if ( substr( $remote_address, 0, 7 ) === '192.168' ) {
+    $is_local = true;
+  } elseif ( in_array( $_SERVER['REMOTE_ADDR'], $localhost_whitelist ) ) {
+    $is_local = true;
+  }
+
+  return $is_local;
 }
