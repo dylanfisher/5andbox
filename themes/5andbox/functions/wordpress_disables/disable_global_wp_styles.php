e5c384b5db74ec72316115af36be72198fe2d3d2
dylanfisher <hi@dylanfisher.com>
2022-05-22 16:32:23 -0400

    Disable WP global style CSS properties

---
+++ themes/5andbox/functions/wordpress_disables/disable_global_wp_styles.php
@@ -0,0 +1,6 @@
+<?php
+
+// https://github.com/WordPress/gutenberg/issues/36834
+
+remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
