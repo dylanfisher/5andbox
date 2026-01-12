05dc9dc03e07bc75d25512d1a3f78b984cc3f0f4
dylanfisher <hi@dylanfisher.com>
2026-01-11 17:30:59 -0800

    Update rest api auth message to save myself future debugging headache

--- themes/5andbox/functions/wordpress_security/require_api_authentication.php
+++ themes/5andbox/functions/wordpress_security/require_api_authentication.php
@@ -10,7 +10,7 @@ add_filter( 'rest_authentication_errors', function( $result ) {
   }
 
   if ( ! is_user_logged_in() ) {
-    return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );
+    return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in. This error is located in functions/wordpress_security/require_api_authentication.php.', array( 'status' => 401 ) );
   }
 
   return $result;
